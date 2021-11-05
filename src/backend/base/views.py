11f91fe76ad39d24de7a13de6b59c389da2d2031
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-05 21:45:26 +0530

    Use ChatGroupCreateSerializer

--- src/backend/base/views.py
+++ src/backend/base/views.py
@@ -1,18 +1,20 @@
-from rest_framework.response import Response
-from rest_framework import viewsets, status
 from django.shortcuts import get_object_or_404
 from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 
-from .serializers import UserSerializer, ChannelSerializer, ChatGroupSerializer, MemberSerializer, MessageSerializer
-from .models import ChatGroup, Channel, Member, Message
+
+from .serializers import UserSerializer, ChannelSerializer, ChatGroupSerializer, MemberSerializer, InviteSerializer, MessageSerializer
+from .models import ChatGroup, Invite, Channel, Member, Message
+from .extra_serializers import ChatGroupCreateSerializer
+
 
 User = get_user_model()
 
 
 class ChatGroupViewSet(viewsets.ModelViewSet):
     serializer_class = ChatGroupSerializer
-    REQUIRED_FIELDS = {"name", "description"}
-    ALL_EDITABLE_FIELDS = {"name", "description", "icon"}
 
     def get_queryset(self):
         for member in self.request.user.member_set.all():
@@ -33,22 +35,8 @@ class ChatGroupViewSet(viewsets.ModelViewSet):
         return Response(status=status.HTTP_403_FORBIDDEN)
 
     def create(self, request):
-        REQUIRED_FIELDS = self.REQUIRED_FIELDS
-        if REQUIRED_FIELDS & set(request.data) != REQUIRED_FIELDS:
-            return Response(
-                {"detail": f"Required fields not supplied ({', '.join(REQUIRED_FIELDS - set(request.data))})"},
-                status=status.HTTP_400_BAD_REQUEST)
-        icon = request.FILES.get("icon")
-        data = request.data
-        chat_group = ChatGroup.objects.create(name=data["name"], description=data["description"], creator=request.user,
-                                              icon=icon)
-        return Response(self.get_serializer(instance=chat_group).data)
-
-    def partial_update(self, request, pk):
-        chat_group = self.get_chat_group(pk=pk)
-        for key, value in request.data.items():
-            if key in self.ALL_EDITABLE_FIELDS:
-                setattr(chat_group, key, value)
-
-        chat_group.save(force_update=True)
+        data = {**request.data, "creator": UserSerializer(request.user).data}
+        serializer = ChatGroupCreateSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        chat_group = serializer.save()
         return Response(self.get_serializer(instance=chat_group).data)
