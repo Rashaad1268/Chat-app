b62c1d55f7f810ce6e9fc2ec34c13412e4f860b0
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-13 17:52:31 +0530

    Added ChannelViewSet to the routes

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework import routers
 
-from .views import ChatGroupViewSet
+from .views import ChatGroupViewSet, ChannelViewSet
 
 
 router = routers.DefaultRouter()
 router.register("chat-groups", ChatGroupViewSet, basename="chat_group_viewset")
+router.register("channels", ChannelViewSet, basename="channel_viewset")
 
 urlpatterns = router.urls
