677e04296c869573dec8f07c7a5c5553acf2444d
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-13 16:52:41 +0530

    Added signals.py

---
+++ src/backend/base/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import ChatGroup, Invite, Member, Channel
+
+
+@receiver(post_save, sender=ChatGroup)
+def create_chat_group(sender, instance, created, **kwargs):
+    """Creates a Channel, Invite, Member object when a ChatGroup is created"""
+    if created:
+        Channel.objects.create(chat_group=instance, name="General", position=0)
+        Invite.objects.create(chat_group=instance)
+        Member.objects.create(user=instance.creator, chat_group=instance)
