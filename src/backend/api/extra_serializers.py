78e9e935803a8281c14c955e721e997f14213ab3
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-13 17:08:21 +0530

    Renamed ChannelCreateSerializer

--- src/backend/api/extra_serializers.py
+++ src/backend/api/extra_serializers.py
@@ -14,15 +14,9 @@ class ChatGroupCreateSerializer(ModelSerializer):
         model = ChatGroup
         fields = ("creator", "name", "description", "icon")
 
-    def create(self, data):
-        return ChatGroup.objects.create(creator_id=data.pop("creator"), **data)
 
-
-class ChannelCreate(ModelSerializer):
+class ChannelCreateSerializer(ModelSerializer):
 
     class Meta:
         model = Channel
         fields = ("chat_group", "name", "description", "position")
-
-    def create(self, data):
-        return Channel.objects.create(chat_group_id=data.pop("chat_group"), **data)
