81901738bc1972e2d3179f91037f3c20fcb48227
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-13 18:48:11 +0530

    Added MessageCreateSerializer

--- src/backend/api/extra_serializers.py
+++ src/backend/api/extra_serializers.py
@@ -24,7 +24,6 @@ class ChannelCreateSerializer(ModelSerializer):
 
 
 class MessageCreateSerializer(ModelSerializer):
-    author = serializers.IntegerField(required=True)
     class Meta:
         model = Message
         fields = ("author", "channel", "content")
