ddaafbf3f62c0280f39e549078ba26ed2dc5cf47
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-27 17:35:35 +0530

    Added websocket_urlpatterns

--- src/backend/api/urls.py
+++ src/backend/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework_nested.routers import NestedDefaultRouter
 
 from .views import ChatGroupViewSet, ChannelViewSet, MessageViewSet
 
+from .consumers import ChatConsumer
 
 router = routers.DefaultRouter()
 router.register("chat-groups", ChatGroupViewSet, basename="chat_group_viewset")
@@ -13,3 +14,7 @@ message_router = NestedDefaultRouter(router, "channels", lookup="channel")
 message_router.register("messages", MessageViewSet, basename="message_viewset")
 
 urlpatterns = router.urls + message_router.urls
+
+websocket_urlpatterns = [
+    path("api/ws/", ChatConsumer.as_asgi())
+]
