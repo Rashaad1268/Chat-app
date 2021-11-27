75ecf0924b274570d13de8e872e9959649af958d
Rashaad Akbar <rashaadzma@gmail.com>
2021-11-27 17:36:00 +0530

    Added ChatConsumer

--- src/backend/api/consumers.py
+++ src/backend/api/consumers.py
@@ -1,17 +1,30 @@
 import json
 from channels.generic.websocket import WebsocketConsumer
 
+from api.serializers import UserSerializer
+
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        self.accept()
+        user = self.scope["user"]
+        if not user.is_anonymous:
+            self.channel_name = f"User({user.id})"
+            self.accept()
+            self.send(text_data=json.dumps(UserSerializer(user).data))
+        else:
+            self.close()
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
+        data = json.loads(text_data)
 
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+    def chat_message_send(self, event):
+        self.send(text_data=json.dumps(event["message"]))
+
+    def chat_message_delete(self, event):
+        self.send(text_data=json.dumps(event["message"]))
+
+    def chat_message_edit(self, event):
+        self.send(text_data=json.dumps(event["message"]))
