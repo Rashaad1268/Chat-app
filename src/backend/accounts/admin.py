791459c16921abe6cc82336ca4b24efd2fd5e661
Rashaad Akbar <rushda2000@gmail.com>
2022-02-07 12:54:31 +0530

    feat(Admin panel): Add is_online field

--- src/backend/accounts/admin.py
+++ src/backend/accounts/admin.py
@@ -5,6 +5,13 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     readonly_fields = ("date_joined",)
+    list_display = UserAdmin.list_display + ("is_online",)
+    list_filter = UserAdmin.list_filter + ("is_online",)
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Custom fields", {
+            "fields": ("is_online",),
+        }),
+    )
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
