1e10065409b077558432e7c3c7fadf3f571cd9f3
Rashaad Akbar <rushda2000@gmail.com>
2022-02-07 12:56:44 +0530

    fix(Serializers): Bugfixes

--- src/backend/api/serializers.py
+++ src/backend/api/serializers.py
@@ -1,17 +1,21 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
-from django.contrib.auth import get_user_model
+
 from base.models import ChatGroup, Invite, Channel, Member, Message
 
 
 class UserSerializer(ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "date_joined", "profile_picture")
+        fields = ("id", "username", "date_joined", "is_online", "profile_picture")
 
 
 class MemberSerializer(ModelSerializer):
     user = UserSerializer()
+    chat_group = serializers.PrimaryKeyRelatedField(allow_null=False,
+                                                    read_only=True,
+                                                    pk_field=serializers.UUIDField(format='hex_verbose'))
 
     class Meta:
         model = Member
@@ -19,6 +23,10 @@ class MemberSerializer(ModelSerializer):
 
 
 class ChannelSerializer(ModelSerializer):
+    chat_group = serializers.PrimaryKeyRelatedField(allow_null=False,
+                                                    read_only=True,
+                                                    pk_field=serializers.UUIDField(format='hex_verbose'))
+
     class Meta:
         model = Channel
         fields = "__all__"
@@ -31,6 +39,8 @@ class InviteSerializer(ModelSerializer):
 
 
 class ChatGroupSerializer(ModelSerializer):
+    id = serializers.SerializerMethodField()
+    creator = UserSerializer()
     members = MemberSerializer(many=True)
     channels = ChannelSerializer(many=True)
     invite = InviteSerializer()
@@ -39,9 +49,22 @@ class ChatGroupSerializer(ModelSerializer):
         model = ChatGroup
         fields = "__all__"
 
+    def get_id(self, chat_group):
+        return str(chat_group.id)
+
 
 class MessageSerializer(ModelSerializer):
-    author = MemberSerializer()
+    author = serializers.SerializerMethodField()
+    channel = serializers.SerializerMethodField()
+    chat_group = serializers.PrimaryKeyRelatedField(allow_null=False,
+                                                    read_only=True,
+                                                    pk_field=serializers.UUIDField(format='hex_verbose'))
+
+    def get_author(self, message):
+        return MemberSerializer(message.author).data
+
+    def get_channel(self, message):
+        return ChannelSerializer(message.channel).data
 
     class Meta:
         model = Message
