f9ff4d6da42d646dd43d1f046057d846b4702197
Rashaad Akbar <rushda2000@gmail.com>
2022-02-10 17:58:28 +0530

    feat: Added RoleCreateSerializer

--- src/backend/api/extra_serializers.py
+++ src/backend/api/extra_serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
 
-from base.models import ChatGroup, Channel, Message
+from base.models import ChatGroup, Channel, Message, Role
 from .serializers import UserSerializer, MemberSerializer
 
 
@@ -14,6 +14,15 @@ class ChatGroupCreateSerializer(ModelSerializer):
     class Meta:
         model = ChatGroup
         fields = ("creator", "name", "description", "icon")
+
+
+class RoleCreateSerializer(ModelSerializer):
+    position = serializers.IntegerField(read_only=True)
+    permissions = serializers.DictField(child=serializers.BooleanField(), allow_empty=True)
+
+    class Meta:
+        model = Role
+        fields = ("__all__",)
 
 
 class ChannelCreateSerializer(ModelSerializer):
