fa71f0bd199b15805c6d1e17e0b4c97051f016f3
Rashaad Akbar <rushda2000@gmail.com>
2022-02-28 14:45:51 +0530

    feat: Add token verification endpoint

--- src/backend/accounts/urls.py
+++ src/backend/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 from .views import SignupView, CustomTokenObtainPairView, UserViewSet
 
@@ -12,4 +12,5 @@ urlpatterns = [
     path("signup/", SignupView.as_view(), name="signup"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name='token_verify'),
 ] + router.urls
