b4edd44ebafc78d7dad18798f41a8d536128a1b3
Rashaad Akbar <rushda2000@gmail.com>
2022-03-29 12:12:27 +0530

    fix: bug fixes

--- src/mobile_frontend/lib/widgets/home_page.dart
+++ src/mobile_frontend/lib/widgets/home_page.dart
@@ -1,40 +1,51 @@
-import 'dart:convert' show Encoding, json;
+import 'dart:convert' show json;
 import 'package:flutter/material.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart' as http;
 import 'package:mobile_frontend/widgets/authentication.dart';
-import '../constants.dart' show apiUrl;
-
-const secureStorage = FlutterSecureStorage();
+import 'package:mobile_frontend/api.dart';
+import '../constants.dart' show secureStorage;
 
 class HomePage extends StatefulWidget {
-  HomePage({Key? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  Map<String, dynamic> userData = {};
   Map<int, dynamic> chatGroups = {};
-  bool isLoggedIn = false;
+  bool isLoggedIn = true;
 
-  _HomePageState() {
+  @override
+  void initState() {
+    super.initState();
+
     verifyToken().then((bool result) => {setIsLoggedIn(result)});
+    secureStorage.read(key: 'userData').then((data) => setState((() {
+          if (data != null) {
+            userData = json.decode(data);
+          } else {
+            // the user data does not exist for some reason
+            setIsLoggedIn(false); // so make the user login again
+          }
+        })));
   }
 
   Future<bool> verifyToken() async {
-    final token = await secureStorage.read(key: "accessToken");
-    if (token == null) {
+    final tokenString = await secureStorage.read(key: 'userData');
+
+    if (tokenString == null) return false;
+
+    final tokens = json.decode(tokenString)['tokens'];
+
+    if (tokens == null) {
       return false;
     }
 
-    var response = await http.post(Uri.parse(apiUrl + "auth/token/verify/"),
-        body: json.encode({"token": token}),
-        headers: <String, String>{
-          'Content-Type': 'application/json; charset=UTF-8',
-        });
+    var response = await requestApi('post', 'auth/token/verify/',
+        data: {'token': tokens['access']});
 
-    return response.body == "{}";
+    return response.data.toString() == "{}";
   }
 
   void setIsLoggedIn(bool status) {
@@ -45,15 +56,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    print("Login Status " + isLoggedIn.toString());
     if (isLoggedIn == false) {
       return LoginPage(setIsLoggedIn);
     }
-    return const Scaffold(
+
+    String username = userData['username']
+        .toString(); // userData['username'] maybe null so lets cast it to string
+
+    return Scaffold(
         body: Align(
             child: Text(
-              'Home page',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              "Hello $username",
+              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             heightFactor: 15));
   }
