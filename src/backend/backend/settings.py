df9cf6c52863ffe8b430c8700f32f6f31a9d3c6c
Rashaad Akbar <rushda2000@gmail.com>
2022-04-21 22:42:44 +0530

    feat: Update token lifespan

--- src/backend/backend/settings.py
+++ src/backend/backend/settings.py
@@ -182,8 +182,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),  # For debugging purposes
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=20),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
     'UPDATE_LAST_LOGIN': False,
