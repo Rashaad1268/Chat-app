7dccd1870112176e6067613ca00a61a4333740d1
Rashaad Akbar <rushda2000@gmail.com>
2022-04-24 00:33:28 +0530

    refactor: move file here

---
+++ src/mobile_frontend/lib/utils/constants.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+const theme = MaterialColor(
+  0xFF0D47A1,
+  <int, Color>{
+    50: Color(0xFFE3F2FD),
+    100: Color(0xFFBBDEFB),
+    200: Color(0xFF90CAF9),
+    300: Color(0xFF64B5F6),
+    400: Color(0xFF42A5F5),
+    500: Color(0xFF2196F3),
+    600: Color(0xFF1E88E5),
+    700: Color(0xFF1976D2),
+    800: Color(0xFF1565C0),
+    900: Color(0xFF0D47A1),
+  },
+);
+
+const secureStorage = FlutterSecureStorage();
+
+const apiUrl = 'http://127.0.0.1:8000/api/';
+const websocketUrl = 'ws://127.0.0.1:8000/api/ws/';
+
+final emailRegex = RegExp(
+    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$');
