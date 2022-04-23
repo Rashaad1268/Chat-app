8b77f870f98f07f7ab7133b23265b301e206309c
Rashaad Akbar <rushda2000@gmail.com>
2022-04-24 00:39:09 +0530

    refactor: Add drawer.dart

---
+++ src/mobile_frontend/lib/widgets/drawer.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+
+class DrawerTile extends StatelessWidget {
+  Map<String, dynamic> chatGroupData;
+  DrawerTile(this.chatGroupData, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(chatGroupData['name']),
+      leading: CircleAvatar(
+        backgroundImage:
+            NetworkImage('http://127.0.0.1:8000${chatGroupData["icon"]}'),
+      ),
+    );
+  }
+}
+
+class ChatGroupDrawer extends StatefulWidget {
+  final List chatGroups;
+  const ChatGroupDrawer(this.chatGroups, {Key? key}) : super(key: key);
+
+  @override
+  State<ChatGroupDrawer> createState() => _ChatGroupDrawerState();
+}
+
+class _ChatGroupDrawerState extends State<ChatGroupDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    // print(widget.chatGroups);
+    return Drawer(
+        child: ListView(
+      children: widget.chatGroups.map((d) => DrawerTile(d)).toList(),
+    ));
+  }
+}
