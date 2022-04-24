e30f315e575b7e1534418851b03e027542ceff1b
Rashaad Akbar <rushda2000@gmail.com>
2022-04-24 13:35:16 +0530

    feat: add chat_group_list.dart

---
+++ src/mobile_frontend/lib/widgets/chat_group_list.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+import '../pages/chat_group_detail.dart';
+
+class Tile extends StatelessWidget {
+  final Map chatGroupData;
+  const Tile(this.chatGroupData, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: ListTile(
+      title: Text(chatGroupData['name']),
+      onTap: () {
+        Navigator.of(context).push(
+            MaterialPageRoute(
+                builder: (ctx) => ChatGroupDetail(chatGroupData)));
+      },
+      leading: CircleAvatar(
+        backgroundImage:
+            NetworkImage('http://127.0.0.1:8000${chatGroupData["icon"]}'),
+      ),
+    ));
+  }
+}
+
+class ChatGroupList extends StatelessWidget {
+  final List chatGroups;
+  const ChatGroupList(this.chatGroups, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final diviededTiles = ListTile.divideTiles(
+        context: context, tiles: chatGroups.map((e) => Tile(e))).toList();
+    return ListView(
+      padding: const EdgeInsets.all(14),
+      children: diviededTiles,
+    );
+  }
+}
