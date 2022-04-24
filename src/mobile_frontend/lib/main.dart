2e5c559990c65df15fc119a2f1c00da97a3fda47
Rashaad Akbar <rushda2000@gmail.com>
2022-04-24 13:08:18 +0530

    remove debug sign

--- src/mobile_frontend/lib/main.dart
+++ src/mobile_frontend/lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage()
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
       );
   }
 }
